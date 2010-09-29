88a43d5079e6b311cc5d0e68210de9781ca4e171
Jonas Haag <jonas@lophus.org>
2010-09-29 17:54:29 +0200

    Fixed tests

--- tests.py
+++ tests.py
@@ -4,14 +4,14 @@ import stupff
 class UtilsTestcase(unittest.TestCase):
     def test_nice_percent(self):
         for inp, outp in (
-            [(4, 2), 50],
-            [(1.0, 3.339), 33],
+            [(2, 4), 50],
+            [(0.339, 1), 33],
             [(1000, 3), 100]
         ):
             self.assertEqual(stupff.nice_percent(*inp), outp)
 
     def test_autosize(self):
-        for digits in (None, 1, 2, 3):
+        for digits in (1, 2, 3):
             for size, max_size, outp in (
                 [(400, 300), (400, 300), (400, 300)],
                 [(360, 150), (40, 999),  (40, 16.666)],
@@ -22,9 +22,9 @@ class UtilsTestcase(unittest.TestCase):
                 class video:
                     width, height = size
                 w, h = stupff.autosize(video, *max_size)
-                if digits is not None:
-                    outp = map(lambda x:round(x, digits), outp)
-                self.assertEqual((w, h), outp)
+                outp = [round(x, digits) for x in outp]
+                w_h = [round(x, digits) for x in outp]
+                self.assertEqual(w_h, outp)
 
 if __name__ == '__main__':
     unittest.main()
